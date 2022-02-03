cf0bf5d344bf00fe93547d9f361c962e4e1a9124
Edmilson <chvez4012@gmail.com>
2022-02-03 16:29:37 -0500

    v.1

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+const ico_menu = document.querySelector(".ico__menu")
+const  menu =  document.querySelector(".menu")
+
+    ico_menu.addEventListener('click',()=>{
+        menu.classList.toggle("mostrar")
+    })
+
